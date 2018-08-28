d1e9a0d5b91f67bbcc813d359e289c01024b9b7f
Toby Fleming <tobflem@amazon.com>
2018-08-28 14:28:28 -0700

    Java generation to use pathlib, logging

--- uluru/generators/java.py
+++ uluru/generators/java.py
@@ -1,94 +1,93 @@
-import os
+# pylint: disable=too-many-locals
+import logging
+from pathlib import Path
 
-from uluru.filters import resource_service_name, resource_type_name
+LOG = logging.getLogger(__name__)
 
 
 def generate(env, resource_def, project_settings):
-    resource_name = resource_type_name(resource_def["Type"])
-    curdl = ["Create", "Update", "Read", "Delete", "List"]
-    output_directory = project_settings["output_directory"]
+    namespace, service_name, resource_name = resource_def["Type"].split("::")
+
+    curdl = ("Create", "Update", "Read", "Delete", "List")
+    output_directory = Path(project_settings["output_directory"]).resolve(strict=True)
     package_name_prefix = project_settings["PackageNamePrefix"]
-    project_directory = (
-        package_name_prefix
-        if package_name_prefix == package_name_prefix.replace(".", "/")
-        else package_name_prefix.replace(".", "/")
-    )
 
     project_settings.setdefault("Client", {})
     java_client_keys = ["Client", "Builder", "ResourceModel"]
     for key in java_client_keys:
         project_settings["Client"].setdefault(key, key.upper())
 
-    source_directory = os.path.join(output_directory, "src", project_directory)
-    test_directory = os.path.join(output_directory, "tst")
+    project_directory = package_name_prefix.split(".")
 
-    print("Creating output directory...")
-    os.system(
-        "mkdir -p "
-        + source_directory
-        + "/{handlers,models,utils} "
-        + test_directory
-        + "/{unit,integration}"
-    )
+    source_directory = output_directory.joinpath("src", *project_directory)
+    test_directory = output_directory.joinpath("tst", *project_directory)
 
-    # destination files
-    handler_file = os.path.join(source_directory, "handlers", "{}{}Handler.java")
-    # % (resource name, handler_type)
-    models_file = os.path.join(source_directory, "models/{}Model.java")
-    # % resource_name
-    utils_file = os.path.join(source_directory, "utils/{}.java")
-    # % utils_class_name
-    unit_file = os.path.join(test_directory, "unit/{}HandlerUnitTests.java")
-    # % handler_type
-    unit_testbase_file = os.path.join(test_directory, "unit/TestBase.java")
-    integ_file = os.path.join(test_directory, "integration/{}IntegrationTests.java")
-    # % resource_name
-    # not sure how to format long line above
+    LOG.info("Creating output directory...")
+    for dir_name in ("handlers", "models", "utils"):
+        directory = source_directory / dir_name
+        LOG.debug("Creating %s", directory)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    for dir_name in ("unit", "integration"):
+        directory = test_directory / dir_name
+        LOG.debug("Creating %s", directory)
+        directory.mkdir(parents=True, exist_ok=True)
 
     # dictionary maps template_path: output_path
-    templates_and_outputs = {
-        "handlers/{}Handler.java".format(handler_type): handler_file.format(
-            resource_name, handler_type
+    templates_and_outputs = [
+        (
+            "models/ResourceModel.java",
+            source_directory / "models" / "{}Model.java".format(resource_name),
+        ),
+        (
+            "utils/ClientBuilder.java",
+            source_directory / "utils" / "{}ClientBuilder.java".format(service_name),
+        ),
+        (
+            "utils/ResourceResponseReturner.java",
+            source_directory / "utils" / "{}.java".format("ResourceResponseReturner"),
+        ),
+        ("unit/TestBase.java", test_directory / "unit" / "TestBase.java"),
+        (
+            "integration/IntegrationTests.java",
+            test_directory
+            / "integration"
+            / "{}IntegrationTests.java".format(resource_name),
+        ),
+    ]
+    templates_and_outputs.extend(
+        (
+            "handlers/{}Handler.java".format(handler_type),
+            source_directory
+            / "handlers"
+            / "{}{}Handler.java".format(resource_name, handler_type),
         )
         for handler_type in curdl
-    }  # not sure how to format long line above
-    templates_and_outputs.update(
-        {
-            "models/ResourceModel.java": models_file.format(resource_name),
-            "utils/ClientBuilder.java": utils_file.format(
-                resource_service_name(resource_def["Type"]) + "ClientBuilder"
-            ),
-            "utils/ResourceResponseReturner.java": utils_file.format(
-                "ResourceResponseReturner"
-            ),
-            "unit/TestBase.java": unit_testbase_file,
-            "integration/IntegrationTests.java": integ_file.format(resource_name),
-        }
     )
-    templates_and_outputs.update(
-        {
-            "unit/{}HandlerUnitTests.java".format(handler_type): unit_file.format(
-                handler_type
-            )
-            for handler_type in curdl
-        }
-    )  # not sure how to format long lines above
+    templates_and_outputs.extend(
+        (
+            "unit/{}HandlerUnitTests.java".format(handler_type),
+            test_directory
+            / "unit"
+            / "{}{}HandlerUnitTests.java".format(resource_name, handler_type),
+        )
+        for handler_type in curdl
+    )
 
     # writes a jinja subclass to the templates folder and adds the subresource
     # template:output pair to the dictionary.
+    template_path = "models/ResourceModel.java"
+    template = env.get_template(template_path)
     try:
         for definition_name, definition_properties in resource_def[
             "Definitions"
         ].items():
             # not sure how to format long line above
             definition_properties = definition_properties["Properties"]
-            template_path = "models/ResourceModel.java"
-            template = env.get_template(template_path)
-            def_output_path = os.path.join(
-                source_directory, "models/{}Model.java".format(definition_name)
+            def_output_path = (
+                source_directory / "models" / "{}Model.java".format(definition_name)
             )
-            # not sure how to format long line above
-            with open(def_output_path, "w", encoding="utf-8") as f:
+            with def_output_path.open("w", encoding="utf-8") as f:
                 f.write(
                     template.render(
                         **resource_def,
@@ -97,17 +96,19 @@ def generate(env, resource_def, project_settings):
                         resource_properties=definition_properties,
                     )
                 )
-    except KeyError as e:
-        print("ERROR: " + e)
+    except KeyError:
+        LOG.exception("ERROR")  # what would cause this?
 
-    for template_path, output_path in templates_and_outputs.items():
+    for template_path, output_path in templates_and_outputs:
         template = env.get_template(template_path)
-        with open(output_path, "w", encoding="utf-8") as f:
+        with output_path.open("w", encoding="utf-8") as f:
             f.write(
                 template.render(
                     **resource_def,
                     **project_settings,
-                    resource_name=resource_type_name(resource_def["Type"]),
+                    namespace=namespace,
+                    service_name=service_name,
+                    resource_name=resource_name,
                     resource_properties=resource_def["Properties"],
                 )
             )
