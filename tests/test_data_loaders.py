c43c53a48b4368ba8f78d0c521fabe68ca55e0ca
Toby Fleming <tobflem@amazon.com>
2018-08-30 15:01:53 -0700

    Fix unittests and enable for CI

--- tests/test_data_loaders.py
+++ tests/test_data_loaders.py
@@ -1,4 +1,5 @@
 from io import StringIO
+from pathlib import Path
 
 import jsonschema
 import pytest
@@ -7,20 +8,22 @@ import yaml
 from uluru.data_loaders import load_project_settings, load_resource_spec
 
 
-def test_load_resource_spec_json_error():
-    with pytest.raises(yaml.parser.ParserError):
+def test_load_resource_spec_not_yaml():
+    with pytest.raises(yaml.YAMLError):
         load_resource_spec(StringIO("}"))
 
 
-def test_load_resource_spec_invalid_spec():
-    # TODO: our spec is too lenient
-    load_resource_spec(StringIO("{}"))
+def test_load_resource_spec_empty_is_invalid():
+    with pytest.raises(jsonschema.exceptions.ValidationError):
+        load_resource_spec(StringIO(""))
 
 
-def test_load_resource_spec_valid_spec():
-    spec = {}
-    file_like = StringIO(yaml.dump(spec))
-    assert load_resource_spec(file_like) == spec
+def test_load_resource_spec_example_spec_is_valid():
+    basedir = Path(__file__).parent.parent  # tests/test_data_loaders.py
+    example = basedir / "examples" / "aws-kinesis-stream.json"
+    with example.open("r", encoding="utf-8") as f:
+        spec = load_resource_spec(f)
+    assert spec
 
 
 def test_load_project_settings_java_defaults():
@@ -34,9 +37,13 @@ def test_load_project_settings_java_user_specified_valid():
     assert merged_settings["PackageNamePrefix"] == "org.my.package"
 
 
+def test_load_project_settings_java_user_specified_not_yaml():
+    with pytest.raises(yaml.YAMLError):
+        load_project_settings("java", StringIO("}"))
+
+
 def test_load_project_settings_java_user_specified_invalid():
     user_settings = {"PackageNamePrefix": {}}
     file_like = StringIO(yaml.dump(user_settings))
-
     with pytest.raises(jsonschema.exceptions.ValidationError):
         load_project_settings("java", file_like)
