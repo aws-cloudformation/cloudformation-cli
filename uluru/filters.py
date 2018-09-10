f866dcbfc2df44200f5af31546261d6befced002
John Tompkins <jotompki@amazon.com>
2018-09-10 13:22:39 -0700

    Package prefix and name are now one property in settings

--- uluru/filters.py
+++ uluru/filters.py
@@ -153,3 +153,25 @@ def modified_from_action_type(action_type):
     if action_type == "write":
         return "true"
     return "false"
+
+
+@register_filter
+def package_prefix(full_package_name):
+    """Maps JSON schema types to Java types. If the type cannot be mapped, it
+        is returned as is.
+
+        >>> package_prefix('com.example.test')
+        'com.example'
+        >>> package_prefix('example.test')
+        'example'
+        >>> package_prefix('com.example.this.isa.test')
+        'com.example.this.isa'
+        >>> package_prefix('exampletest')
+        Traceback (most recent call last):
+        ...
+        ValueError: Package name 'exampletest' is invalid
+        """
+    package_segments = full_package_name.rpartition(".")
+    if package_segments[0]:
+        return package_segments[0]
+    raise ValueError("Package name '{}' is invalid".format(full_package_name))
