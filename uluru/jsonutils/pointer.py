b4e16af9d8538b9e8d941454ce5be2d239afc233
Toby Fleming <tobflem@amazon.com>
2018-10-03 21:06:21 -0700

    Inline remote references into the base schema

---
+++ uluru/jsonutils/pointer.py
@@ -0,0 +1,97 @@
+"""Encode JavaScript Object Notation (JSON) Pointer as per
+`RFC-6901 <https://tools.ietf.org/html/rfc6901>`_.
+"""
+from itertools import chain
+from urllib.parse import quote, unquote
+
+
+def part_encode(part):
+    """Encode a part of a JSON pointer.
+
+    >>> part_encode("foo")
+    'foo'
+    >>> part_encode("~foo")
+    '~0foo'
+    >>> part_encode("foo~")
+    'foo~0'
+    >>> part_encode("/foo")
+    '~1foo'
+    >>> part_encode("foo/")
+    'foo~1'
+    >>> part_encode("f/o~o")
+    'f~1o~0o'
+    >>> part_encode("~0")
+    '~00'
+    >>> part_encode("~1")
+    '~01'
+    >>> part_encode(0)
+    '0'
+    """
+    return str(part).replace("~", "~0").replace("/", "~1")
+
+
+def part_decode(part):
+    """Decode a part of a JSON pointer.
+
+    >>> part_decode("foo")
+    'foo'
+    >>> part_decode("~0foo")
+    '~foo'
+    >>> part_decode("foo~0")
+    'foo~'
+    >>> part_decode("~1foo")
+    '/foo'
+    >>> part_decode("foo~1")
+    'foo/'
+    >>> part_decode("f~1o~0o")
+    'f/o~o'
+    >>> part_decode("~00")
+    '~0'
+    >>> part_decode("~01")
+    '~1'
+    >>> part_decode("0")
+    '0'
+    """
+    return part.replace("~1", "/").replace("~0", "~")
+
+
+def fragment_encode(parts, prefix="#"):
+    """Encode all parts of a JSON pointer into the URI fragment
+    identifier representation.
+
+    >>> fragment_encode([])
+    '#'
+    >>> fragment_encode([], prefix="")
+    ''
+    >>> fragment_encode(["foo", "bar"])
+    '#/foo/bar'
+    >>> fragment_encode([0, " ", "~"])
+    '#/0/%20/~0'
+    """
+    encoded = (quote(part_encode(part), safe="/~") for part in parts)
+    return "/".join(chain([prefix], encoded))
+
+
+def fragment_decode(pointer, prefix="#", output=tuple):
+    """Decode all segments of a JSON pointer from the URI fragment
+    identifier representation.
+
+    >>> fragment_decode("#")
+    ()
+    >>> fragment_decode("#/foo/bar")
+    ('foo', 'bar')
+    >>> fragment_decode("#/foo/bar", output=list)
+    ['foo', 'bar']
+    >>> fragment_decode("#/0/%20/~0")
+    ('0', ' ', '~')
+    >>> fragment_decode("/foo")
+    Traceback (most recent call last):
+    ...
+    ValueError: Expected prefix '#', but was ''
+    """
+    segments = pointer.split("/")
+    decoded = (part_decode(unquote(segment)) for segment in segments)
+    actual = next(decoded)
+    if prefix != actual:
+        raise ValueError("Expected prefix '{}', but was '{}'".format(prefix, actual))
+    return output(decoded)
---
+++ uluru/jsonutils/renamer.py
@@ -0,0 +1,49 @@
+from itertools import count
+
+from jsonschema.compat import urldefrag
+
+from .pointer import fragment_decode
+
+
+class RefRenamer:
+    def __init__(self, renames=None):
+        self.renames = renames if renames else {}
+        # this generator never completes
+        self.names = ("schema{}".format(i) for i in count())  # pragma: no cover
+
+    def items(self):
+        """Return all renames.
+
+        >>> r = RefRenamer()
+        >>> r.items()
+        dict_items([])
+        """
+        return self.renames.items()
+
+    def parse_ref_url(self, ref):
+        """Parse a reference URL into a tuple of base + parts.
+
+        Schemas are renamed for easier processing.
+
+        >>> r = RefRenamer({"file://base.json": "base"})
+        >>> sorted(r.items())
+        [('file://base.json', 'base')]
+        >>> r.parse_ref_url("file://base.json")
+        ('base',)
+        >>> r.parse_ref_url("file://base.json#/foo/bar")
+        ('base', 'foo', 'bar')
+        >>> sorted(r.items())
+        [('file://base.json', 'base')]
+        >>> r.parse_ref_url("file://other.json")
+        ('schema0',)
+        >>> r.parse_ref_url("file://other.json#/foo/bar")
+        ('schema0', 'foo', 'bar')
+        >>> sorted(r.items())
+        [('file://base.json', 'base'), ('file://other.json', 'schema0')]
+        """
+        url, fragment = urldefrag(ref)
+        try:
+            rename = self.renames[url]
+        except KeyError:
+            self.renames[url] = rename = next(self.names)
+        return (rename,) + fragment_decode(fragment, prefix="")
