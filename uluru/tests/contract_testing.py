f0cb8bd3c2cf59bac7e08d3d0f278ace4af51abe
John Tompkins <jotompki@amazon.com>
2018-10-09 13:47:13 -0700

    Removed handler client and made suggested changes

--- uluru/tests/contract_testing.py
+++ uluru/tests/contract_testing.py
@@ -4,10 +4,46 @@ from collections import deque
 from pprint import pprint
 from uuid import uuid4
 
+import boto3
 import pytest
+from botocore import UNSIGNED
+from botocore.config import Config
 from pytest_localserver.http import Request, Response, WSGIServer
 
-from uluru.tests.handler_client import HandlerClient
+
+class InvalidTransportTypeError(Exception):
+    pass
+
+
+def transport_lambda(endpoint):
+    client = boto3.client(
+        "lambda",
+        endpoint_url=endpoint,
+        use_ssl=False,
+        verify=False,
+        config=Config(
+            signature_version=UNSIGNED,
+            read_timeout=5,
+            retries={"max_attempts": 0},
+            region_name="us-east-1",
+        ),
+    )
+
+    def call(request_payload):
+        response = client.invoke(
+            FunctionName="Handler", Payload=json.dumps(request_payload).encode("utf-8")
+        )
+        return json.load(response["Payload"])
+
+    return call
+
+
+def transport(transport_type, endpoint):
+    if transport_type == "lambda":
+        return transport_lambda(endpoint)
+    raise InvalidTransportTypeError(
+        'Transport Type "{}" is not valid.'.format(transport_type)
+    )
 
 
 @pytest.fixture
@@ -45,18 +81,19 @@ def wait_for_terminal_event(listener, timeout_in_seconds=60):
 
 
 def test_simple_create(event_listener):
-    client = HandlerClient("lambda")
+    transport_local = transport("lambda", "http://127.0.0.1:3001")
     _, port = event_listener.server_address
     url = "http://host.docker.internal:{}".format(port)
     token = str(uuid4())
     request = {
         "requestContext": {
             "resourceType": "Dev::Test::Resource",
+            "operation": "Create",
             "clientRequestToken": token,
             "callbackURL": url,
         }
     }
-    client.create(request)
-    is_complete, events = wait_for_terminal_event(event_listener)
-    assert is_complete
+    transport_local(request)
+    is_terminated, events = wait_for_terminal_event(event_listener)
+    assert is_terminated
     assert events[0]["status"] == "COMPLETE"
