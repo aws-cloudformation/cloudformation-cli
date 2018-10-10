d70be70a6e21d68e955cff034c16dca5d6243d9d
Ryan Lohan <ryanloha@amazon.com>
2018-10-10 11:23:41 -0700

    Tightened the provider definition meta schema
        - previous iteration of meta schema was adding very little actual validation over draft-07 and many input documents would validate incorrectly against it, including a single boolean property, various typos, resource definitions with no type name, etc
        - added required 'typeName' property for top-level provider definitions
        - extracted a subset of draft-07 into #/definitions/properties with the following changes from draft-07;
          - `$id` cannot be specified in a resource property
          - `$schema` cannot be specified in a resource property
          - `readOnly` flag is replaced with an array to define semantic meaning and allow $ref's to otherwise-readOnly properties like ARNs
          - `propertyNames` excluded as introduces confusion to template authors
          - `if`, `then`, `else` excluded as introduces confusion to template authors and introduces imperative logic to the form which complicates comprehending the intent. It also didn't actually work in the one place it was being used (to specify mutually exclusive properties, which can be done - and now is - with `allOf`/`anyOf`/`oneOf` constraints)
          - `not` excluded as introduces confusion to template authirs
        - fixed the various samples so they validate against the new meta-schema and corrected some errors
        - attached the draft-07 schema to the common.types definitions which previously had no validation
        - added an initial README.md for the schema to describe and document choices and usage

--- tests/test_awscommon_schema_validation.py
+++ tests/test_awscommon_schema_validation.py
@@ -4,8 +4,9 @@ import json
 from pathlib import Path
 
 import pytest
-from jsonschema import Draft6Validator
 from jsonschema.exceptions import ValidationError
+
+from uluru.data_loaders import make_validator
 
 
 @pytest.fixture
@@ -13,34 +14,20 @@ def schema():
     basedir = Path(__file__).parent.parent
     awscommonschema = basedir / "examples" / "schema" / "aws.common.types.v1.json"
     with awscommonschema.open("r", encoding="utf-8") as f:
-        return json.load(f)
-
-
-def test_accountid_correct(schema):
-    accountid_config = schema["definitions"]["AccountId"]
-    validator = Draft6Validator(accountid_config)
-
-    validator.validate("123456789012")
-
-
-def test_accountid_wrong(schema):
-    accountid_config = schema["definitions"]["AccountId"]
-    validator = Draft6Validator(accountid_config)
-
-    with pytest.raises(ValidationError):
-        validator.validate("1234567890123")
+        resource_schema = json.load(f)
+    return resource_schema
 
 
 def test_arn_correct(schema):
     arn_config = schema["definitions"]["Arn"]
-    validator = Draft6Validator(arn_config)
+    validator = make_validator(arn_config)
 
     validator.validate("arn:aws:rds:eu-west-1:123456789012:db:mysql-db")
 
 
 def test_arn_wrong(schema):
     arn_config = schema["definitions"]["Arn"]
-    validator = Draft6Validator(arn_config)
+    validator = make_validator(arn_config)
 
     with pytest.raises(ValidationError):
         validator.validate("arn:aws:rds:eu-west-1:1234")
@@ -48,60 +35,29 @@ def test_arn_wrong(schema):
 
 def test_availbilityzone_correct(schema):
     availbilityzone_config = schema["definitions"]["AvailabilityZone"]
-    validator = Draft6Validator(availbilityzone_config)
+    validator = make_validator(availbilityzone_config)
 
-    validator.validate("us-west-2b")
+    validator.validate("us-wast-2b")
 
 
 def test_availbilityzone_wrong(schema):
     availbilityzone_config = schema["definitions"]["AvailabilityZone"]
-    validator = Draft6Validator(availbilityzone_config)
+    validator = make_validator(availbilityzone_config)
 
     with pytest.raises(ValidationError):
         validator.validate("us-west")
 
 
-def test_instancetype_correct(schema):
-    instancetype_config = schema["definitions"]["InstanceType"]
-    validator = Draft6Validator(instancetype_config)
-
-    validator.validate("t2.small")
-
-
-def test_instancetype_wrong(schema):
-    instancetype_config = schema["definitions"]["InstanceType"]
-    validator = Draft6Validator(instancetype_config)
-
-    with pytest.raises(ValidationError):
-        validator.validate("t2small")
-
-
 def test_tag_correct(schema):
     tag_config = schema["definitions"]["Tag"]
-    validator = Draft6Validator(tag_config)
+    validator = make_validator(tag_config)
 
     validator.validate({"Key": "123abc", "Value": "123abc"})
 
 
 def test_tag_wrong(schema):
     tag_config = schema["definitions"]["Tag"]
-    validator = Draft6Validator(tag_config)
+    validator = make_validator(tag_config)
 
     with pytest.raises(ValidationError):
         validator.validate({"Key": "aws:123abc", "Value": "aws:123abc"})
-
-
-def test_tag_wrong_missing_key(schema):
-    tag_config = schema["definitions"]["Tag"]
-    validator = Draft6Validator(tag_config)
-
-    with pytest.raises(ValidationError):
-        validator.validate({"Key": "", "Value": "123abc"})
-
-
-def test_tag_wrong_missing_value(schema):
-    tag_config = schema["definitions"]["Tag"]
-    validator = Draft6Validator(tag_config)
-
-    with pytest.raises(ValidationError):
-        validator.validate({"Key": "123abc", "Value": ""})
