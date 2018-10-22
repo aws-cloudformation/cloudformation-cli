6e31385b567c18228fbdec931535319074aa881c
Toby Fleming <tobflem@amazon.com>
2018-10-22 16:32:21 -0700

    Use autospeccing to avoid tests passing with incorrect mock signatures

--- tests/test_subcommands.py
+++ tests/test_subcommands.py
@@ -15,7 +15,7 @@ EXPECTED_PYTEST_ARGS = [
 
 
 def test_test_command():
-    with mock.patch("uluru.test.local_lambda") as mock_lambda_command:
+    with mock.patch("uluru.test.local_lambda", autospec=True) as mock_lambda_command:
         test_file = tempfile.NamedTemporaryFile()
         main(args_in=["test", "local-lambda", test_file.name])
 
@@ -37,7 +37,7 @@ def test_local_lambda_command():
             subparser_name="test",
             test_types=None,
         )
-        with mock.patch("json.load", return_value={}), mock.patch(
+        with mock.patch("json.load", return_value={}, autospec=True), mock.patch(
             "pytest.main"
         ) as mock_pytest:
             local_lambda(arg_namespace)
@@ -55,8 +55,8 @@ def test_local_lambda_with_test_type():
             subparser_name="test",
             test_types="TEST_TYPE",
         )
-        with mock.patch("json.load", return_value={}), mock.patch(
-            "pytest.main"
+        with mock.patch("json.load", return_value={}, autospec=True), mock.patch(
+            "pytest.main", autospec=True
         ) as mock_pytest:
             local_lambda(arg_namespace)
     mock_pytest.assert_called_once()
