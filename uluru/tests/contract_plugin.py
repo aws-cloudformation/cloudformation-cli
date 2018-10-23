2c94dc43c6479509b6b1526fd8d67b4b0e3c2fc5
John Tompkins <jotompki@amazon.com>
2018-10-23 16:20:59 -0700

    Added contract tests involving Create, Read, and Delete

--- uluru/tests/contract_plugin.py
+++ uluru/tests/contract_plugin.py
@@ -2,9 +2,10 @@ import pytest
 
 
 class ContractPlugin:
-    def __init__(self, transport, test_resource):
+    def __init__(self, transport, test_resource, resource_def):
         self._transport = transport
         self._test_resource = test_resource
+        self._resource_def = resource_def
 
     @pytest.fixture
     def test_resource(self):
@@ -13,3 +14,7 @@ class ContractPlugin:
     @pytest.fixture
     def transport(self):
         return self._transport
+
+    @pytest.fixture
+    def resource_def(self):
+        return self._resource_def
