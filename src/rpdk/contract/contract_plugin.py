4a07d18b533ab06a3b9ceb28b477e1a801c8871b
John Tompkins <jotompki@amazon.com>
2018-11-05 16:38:14 -0800

    Added Update specific contract tests and unit tests

--- src/rpdk/contract/contract_plugin.py
+++ src/rpdk/contract/contract_plugin.py
@@ -1,10 +1,19 @@
+import json
+import threading
+from collections import deque
+
 import pytest
+from werkzeug.serving import make_server
+from werkzeug.wrappers import Request, Response
+
+JSON_MIME = "application/json"
 
 
 class ContractPlugin:
-    def __init__(self, transport, test_resource, resource_def):
+    def __init__(self, transport, test_resource, test_updated_resource, resource_def):
         self._transport = transport
         self._test_resource = test_resource
+        self._test_updated_resource = test_updated_resource
         self._resource_def = resource_def
 
     @pytest.fixture
@@ -18,3 +27,42 @@ class ContractPlugin:
     @pytest.fixture
     def resource_def(self):
         return self._resource_def
+
+    @pytest.fixture
+    def test_updated_resource(self):
+        return self._test_updated_resource
+
+    @staticmethod
+    @pytest.fixture
+    def event_listener(request):
+        server = CallbackServer()
+        server.start()
+        request.addfinalizer(server.stop)
+        return server
+
+
+class CallbackServer(threading.Thread):
+    def __init__(self, host="127.0.0.1", port=0):
+        self.events = deque()
+        self._server = make_server(host, port, self, ssl_context=None)
+        self.server_address = self._server.server_address
+        super().__init__(name=self.__class__, target=self._server.serve_forever)
+
+    def __del__(self):
+        self.stop()
+
+    def stop(self):
+        self._server.shutdown()
+
+    @Request.application
+    def __call__(self, request):
+        response = ""
+        content_type = request.headers.get("content-type")
+        if content_type != JSON_MIME:
+            json_response = {
+                "error": 'callback with invalid content type "{}"'.format(content_type)
+            }
+            response = json.dumps(json_response)
+        else:
+            self.events.append(json.loads(request.data))
+        return Response(response, mimetype=JSON_MIME)
--- tests/contract/test_contract_plugin.py
+++ tests/contract/test_contract_plugin.py
@@ -1,11 +1,47 @@
+from unittest.mock import Mock, patch
+
+import requests
+from _pytest.fixtures import FixtureRequest
+from pytest import fixture
+
 from rpdk.contract.contract_plugin import ContractPlugin
+
+
+@fixture
+def listener(request):
+    return ContractPlugin.event_listener(request)
 
 
 def test_contract_plugin_fixtures():
     transport = object()
     test_resource = object()
     resource_def = object()
-    plugin = ContractPlugin(transport, test_resource, resource_def)
+    test_updated_resource = object()
+    plugin = ContractPlugin(
+        transport, test_resource, test_updated_resource, resource_def
+    )
+    request = Mock(spec=FixtureRequest)
+    request.addfinalizer = Mock(return_value=None)
+    with patch("rpdk.contract.contract_plugin.CallbackServer", autospec=True) as server:
+        plugin.event_listener(request)
+    server.assert_called_once()
+    request.addfinalizer.assert_called_once()
+
     assert plugin.transport.__wrapped__(plugin) is transport
     assert plugin.test_resource.__wrapped__(plugin) is test_resource
     assert plugin.resource_def.__wrapped__(plugin) is resource_def
+    assert plugin.test_updated_resource.__wrapped__(plugin) is test_updated_resource
+
+
+def test_callback_server_valid(listener):
+    posted_event = {"event": "test"}
+    requests.post("http://{}:{}".format(*listener.server_address), json=posted_event)
+    recorded_event = listener.events.popleft()
+    assert recorded_event == posted_event
+
+
+def test_callback_server_fail(listener):
+    response = requests.post(
+        "http://{}:{}".format(*listener.server_address), data="Just Text"
+    )
+    assert "callback with invalid content type" in response.json()["error"]
