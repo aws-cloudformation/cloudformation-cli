397ff3bcfa486f56866e594c28a2c4db34ffd1eb
John Tompkins <jotompki@amazon.com>
2018-11-20 11:42:39 -0800

    Converted generator class into functions

--- tests/contract/test_resource_generator.py
+++ tests/contract/test_resource_generator.py
@@ -3,98 +3,84 @@ from collections.abc import Sequence
 
 import pytest
 
-from rpdk.contract.resource_generator import STRING_FORMATS, ResourceGenerator
+from rpdk.contract.resource_generator import STRING_FORMATS, generate_property_strategy
 
 
 @pytest.mark.parametrize("schema_type", ["integer", "number"])
 def test_generate_number_strategy(schema_type):
-    int_schema = {"type": schema_type, "minimum": 0, "maximum": 16}
-    int_strategy = ResourceGenerator._generate_property_strategy(int_schema)
-    generated_int = int_strategy.example()
-    assert int_schema["minimum"] <= generated_int <= int_schema["maximum"]
+    schema = {"type": schema_type, "minimum": 0, "maximum": 16}
+    strategy = generate_property_strategy(schema)
+    assert schema["minimum"] <= strategy.example() <= schema["maximum"]
 
 
 def test_generate_string_regex_strategy():
-    string_schema = {"type": "string", "pattern": "^foo+bar+\\Z$"}
-    regex_strategy = ResourceGenerator._generate_property_strategy(string_schema)
-    generated_string = regex_strategy.example()
-    assert re.fullmatch(string_schema["pattern"], generated_string)
+    schema = {"type": "string", "pattern": "^foo+bar+\\Z$"}
+    regex_strategy = generate_property_strategy(schema)
+    assert re.fullmatch(schema["pattern"], regex_strategy.example())
 
 
 def test_generate_string_format_strategy():
-    string_schema = {"type": "string", "format": "arn"}
-    arn_strategy = ResourceGenerator._generate_property_strategy(string_schema)
-    generated_string = arn_strategy.example()
-    assert re.fullmatch(STRING_FORMATS["arn"], generated_string)
+    schema = {"type": "string", "format": "arn"}
+    strategy = generate_property_strategy(schema)
+    assert re.fullmatch(STRING_FORMATS["arn"], strategy.example())
 
 
 def test_generate_string_strategy():
-    string_schema = {"type": "string", "minLength": 5, "maxLength": 10}
-    string_strategy = ResourceGenerator._generate_property_strategy(string_schema)
-    generated_string = string_strategy.example()
-    assert (
-        string_schema["minLength"]
-        <= len(generated_string)
-        <= string_schema["maxLength"]
-    )
+    schema = {"type": "string", "minLength": 5, "maxLength": 10}
+    strategy = generate_property_strategy(schema)
+    assert schema["minLength"] <= len(strategy.example()) <= schema["maxLength"]
 
 
 def test_generate_boolean_strategy():
-    boolean_schema = {"type": "boolean"}
-    boolean_strategy = ResourceGenerator._generate_property_strategy(boolean_schema)
-    generated_boolean = boolean_strategy.example()
-    assert isinstance(generated_boolean, bool)
+    schema = {"type": "boolean"}
+    strategy = generate_property_strategy(schema)
+    assert isinstance(strategy.example(), bool)
 
 
 def test_generate_array_strategy_simple():
-    array_schema = {"type": "array"}
-    array_strategy = ResourceGenerator._generate_property_strategy(array_schema)
-    generated_list = array_strategy.example()
-    assert isinstance(generated_list, Sequence)
+    schema = {"type": "array"}
+    strategy = generate_property_strategy(schema)
+    assert isinstance(strategy.example(), Sequence)
 
 
 @pytest.mark.parametrize("item_constraint", ["items", "contains"])
 def test_generate_array_strategy_items(item_constraint):
-    array_schema = {"type": "array", item_constraint: {"type": "string"}}
-    array_strategy = ResourceGenerator._generate_property_strategy(array_schema)
-    generated_list = array_strategy.example()
-    assert isinstance(generated_list, Sequence)
-    assert isinstance(generated_list[0], str)
+    schema = {"type": "array", item_constraint: {"type": "string"}}
+    example = generate_property_strategy(schema).example()
+    assert isinstance(example, Sequence)
+    assert isinstance(example[0], str)
 
 
 def test_generate_array_strategy_multiple_items():
-    array_schema = {"type": "array", "items": [{"type": "string"}, {"type": "integer"}]}
-    array_strategy = ResourceGenerator._generate_property_strategy(array_schema)
-    generated_list = array_strategy.example()
-    assert isinstance(generated_list, Sequence)
-    assert isinstance(generated_list[0], str)
-    assert isinstance(generated_list[1], int)
+    schema = {"type": "array", "items": [{"type": "string"}, {"type": "integer"}]}
+    example = generate_property_strategy(schema).example()
+    assert isinstance(example, Sequence)
+    assert isinstance(example[0], str)
+    assert isinstance(example[1], int)
 
 
 def test_generate_one_of_object_strategy():
-    object_schema = {
+    schema = {
         "type": "object",
         "oneOf": [{"required": ["first"]}, {"required": ["second"]}],
         "properties": {"first": {"type": "boolean"}, "second": {"type": "boolean"}},
     }
-    generated_object = ResourceGenerator._generate_property_strategy(
-        object_schema
-    ).example()
+    example = generate_property_strategy(schema).example()
     try:
-        generated_object["first"]
+        example["first"]
     except KeyError:
-        assert isinstance(generated_object["second"], bool)
+        assert isinstance(example["second"], bool)
     else:
-        assert isinstance(generated_object["first"], bool)
+        assert isinstance(example["first"], bool)
 
 
-def test_generate_object_strategy_e2e():
-    object_schema = {
+def test_generate_object_strategy():
+    schema = {
         "type": "object",
         "required": ["first"],
         "properties": {"first": {"type": "boolean"}, "second": {"type": "boolean"}},
     }
-    generated_object = ResourceGenerator(object_schema).generate()
-    assert isinstance(generated_object["first"], bool)
+    example = generate_property_strategy(schema).example()
+    assert isinstance(example["first"], bool)
     with pytest.raises(KeyError):
-        generated_object["second"]
+        example["second"]
