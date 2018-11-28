93386ffc84c824f6fde6c9279fb168581808691b
John Tompkins <jotompki@amazon.com>
2018-11-28 10:27:33 -0800

    Resource Generator (#115)
    * Resource generator that can generate resource examples from schema

---
+++ src/rpdk/contract/resource_generator.py
@@ -0,0 +1,111 @@
+import collections.abc
+
+from hypothesis.strategies import (
+    booleans,
+    characters,
+    decimals,
+    fixed_dictionaries,
+    from_regex,
+    integers,
+    lists,
+    nothing,
+    one_of,
+    text,
+    tuples,
+)
+
+from ..jsonutils.utils import traverse
+
+# TODO This resource generator handles simple cases for resource generation
+# List of outstanding issues available below
+# TODO https://github.com/awslabs/aws-cloudformation-rpdk/issues/118
+
+# Arn is just a placeholder for testing
+STRING_FORMATS = {
+    "arn": "^arn:aws(-(cn|gov))?:[a-z-]+:(([a-z]+-)+[0-9])?:([0-9]{12})?:[^.]+$"
+}
+
+
+def generate_object_strategy(schema):
+    try:
+        one_of_schema = schema["oneOf"]
+    except KeyError:
+        required_properties = schema["required"]
+        strategy = generate_required_properties(schema, required_properties)
+    else:
+        strategies = [
+            generate_required_properties(schema, required_options["required"])
+            for required_options in one_of_schema
+        ]
+        strategy = one_of(strategies)
+    return strategy
+
+
+def generate_required_properties(schema, required):
+    strategies = {
+        prop: generate_property_strategy(traverse(schema, ("properties", prop)))
+        for prop in required
+    }
+    return fixed_dictionaries(strategies)
+
+
+def generate_number_strategy(schema, strategy):
+    minimum = schema.get("minimum")
+    maximum = schema.get("maximum")
+    return strategy(min_value=minimum, max_value=maximum)
+
+
+def generate_string_strategy(schema):
+    try:
+        string_format = schema["format"]
+    except KeyError:
+        min_length = schema.get("minLength", 0)
+        max_length = schema.get("maxLength")
+        strategy = text(
+            alphabet=characters(min_codepoint=1, blacklist_categories=("Cc", "Cs")),
+            min_size=min_length,
+            max_size=max_length,
+        )
+    else:
+        strategy = from_regex(STRING_FORMATS[string_format])
+    return strategy
+
+
+def generate_array_strategy(array_schema):
+    try:
+        item_schema = array_schema["items"]
+    except KeyError:
+        try:
+            item_schema = array_schema["contains"]
+        except KeyError:
+            return lists(nothing())
+    if isinstance(item_schema, collections.abc.Sequence):
+        item_strategy = [generate_property_strategy(schema) for schema in item_schema]
+        # tuples let you define multiple strategies to generate elements.
+        # When more than one schema for an item
+        # is present, we should try to generate both
+        return tuples(*item_strategy)
+    item_strategy = generate_property_strategy(item_schema)
+    return lists(item_strategy, min_size=1)
+
+
+def generate_property_strategy(prop):
+    json_type = prop.get("type", "object")
+    if json_type == "integer":
+        strategy = generate_number_strategy(prop, integers)
+    elif json_type == "number":
+        strategy = generate_number_strategy(prop, decimals)
+    elif json_type == "boolean":
+        strategy = booleans()
+    elif json_type == "string":
+        try:
+            regex = prop["pattern"]
+        except KeyError:
+            strategy = generate_string_strategy(prop)
+        else:
+            return from_regex(regex)
+    elif json_type == "array":
+        strategy = generate_array_strategy(prop)
+    else:
+        strategy = generate_object_strategy(prop)
+    return strategy
---
+++ tests/contract/test_resource_generator.py
@@ -0,0 +1,86 @@
+import re
+from collections.abc import Sequence
+
+import pytest
+
+from rpdk.contract.resource_generator import STRING_FORMATS, generate_property_strategy
+
+
+@pytest.mark.parametrize("schema_type", ["integer", "number"])
+def test_generate_number_strategy(schema_type):
+    schema = {"type": schema_type, "minimum": 0, "maximum": 16}
+    strategy = generate_property_strategy(schema)
+    assert schema["minimum"] <= strategy.example() <= schema["maximum"]
+
+
+def test_generate_string_regex_strategy():
+    schema = {"type": "string", "pattern": "^foo+bar+\\Z$"}
+    regex_strategy = generate_property_strategy(schema)
+    assert re.fullmatch(schema["pattern"], regex_strategy.example())
+
+
+def test_generate_string_format_strategy():
+    schema = {"type": "string", "format": "arn"}
+    strategy = generate_property_strategy(schema)
+    assert re.fullmatch(STRING_FORMATS["arn"], strategy.example())
+
+
+def test_generate_string_strategy():
+    schema = {"type": "string", "minLength": 5, "maxLength": 10}
+    strategy = generate_property_strategy(schema)
+    assert schema["minLength"] <= len(strategy.example()) <= schema["maxLength"]
+
+
+def test_generate_boolean_strategy():
+    schema = {"type": "boolean"}
+    strategy = generate_property_strategy(schema)
+    assert isinstance(strategy.example(), bool)
+
+
+def test_generate_array_strategy_simple():
+    schema = {"type": "array"}
+    strategy = generate_property_strategy(schema)
+    assert isinstance(strategy.example(), Sequence)
+
+
+@pytest.mark.parametrize("item_constraint", ["items", "contains"])
+def test_generate_array_strategy_items(item_constraint):
+    schema = {"type": "array", item_constraint: {"type": "string"}}
+    example = generate_property_strategy(schema).example()
+    assert isinstance(example, Sequence)
+    assert isinstance(example[0], str)
+
+
+def test_generate_array_strategy_multiple_items():
+    schema = {"type": "array", "items": [{"type": "string"}, {"type": "integer"}]}
+    example = generate_property_strategy(schema).example()
+    assert isinstance(example, Sequence)
+    assert isinstance(example[0], str)
+    assert isinstance(example[1], int)
+
+
+def test_generate_one_of_object_strategy():
+    schema = {
+        "type": "object",
+        "oneOf": [{"required": ["first"]}, {"required": ["second"]}],
+        "properties": {"first": {"type": "boolean"}, "second": {"type": "boolean"}},
+    }
+    example = generate_property_strategy(schema).example()
+    try:
+        example["first"]
+    except KeyError:
+        assert isinstance(example["second"], bool)
+    else:
+        assert isinstance(example["first"], bool)
+
+
+def test_generate_object_strategy():
+    schema = {
+        "type": "object",
+        "required": ["first"],
+        "properties": {"first": {"type": "boolean"}, "second": {"type": "boolean"}},
+    }
+    example = generate_property_strategy(schema).example()
+    assert isinstance(example["first"], bool)
+    with pytest.raises(KeyError):
+        example["second"]
