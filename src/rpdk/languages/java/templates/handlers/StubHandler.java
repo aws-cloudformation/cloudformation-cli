7e98dad82d44c73f4d3fe790216a0d982235daff
Ryan Lohan <ryanloha@amazon.com>
2018-12-06 11:11:57 -0800

    Fixed some Java codegen defects.

--- src/rpdk/languages/java/templates/handlers/StubHandler.java
+++ src/rpdk/languages/java/templates/handlers/StubHandler.java
@@ -7,15 +7,18 @@
 package {{ packageName }}.handlers;
 
 import {{ packageName }}.messages.HandlerRequest;
+import {{ packageName }}.messages.HandlerStatus;
 import {{ packageName }}.messages.ProgressEvent;
 import {{ packageName }}.models.{{ pojo_name }};
-import {{ packageName }}.interfaces.ResourceHandler;
 
-public abstract class {{ operation }}Handler extends Base{{ operation }}Handler<{{ pojo_name }}> {
+public class {{ operation }}Handler extends Base{{ operation }}Handler {
 
-    public override ProgressEvent<{{ pojo_name }}> do{{ operation }}(final HandlerRequest<{{ pojo_name }}> request) {
+    @Override
+    public ProgressEvent<{{ pojo_name }}> do{{ operation }}(final HandlerRequest<{{ pojo_name }}> request) {
 
         // TODO : put your code here
+        return new ProgressEvent<{{ pojo_name }}>()
+            .withStatus(HandlerStatus.COMPLETE);
 
     }
 }
