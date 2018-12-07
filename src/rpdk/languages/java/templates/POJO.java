458d424474f438589d0109a277c47d8cfc2a4134
Toby Fleming <tobflem@amazon.com>
2018-12-07 13:45:54 -0800

    Use lombok.Data for POJOs

--- src/rpdk/languages/java/templates/POJO.java
+++ src/rpdk/languages/java/templates/POJO.java
@@ -1,33 +1,16 @@
 // This is a generated file. Modifications will be overwritten.
 package {{ package_name }};
 
+import lombok.Data;
 import java.util.Map;
 import java.util.List;
 import java.util.Set;
 
-public class {{ pojo_name|uppercase_first_letter }} {
 
+@Data
+public class {{ pojo_name|uppercase_first_letter }} {
     {% for name, type in properties.items() %}
     private {{ type }} {{ name|lowercase_first_letter }};
 
     {% endfor %}
-
-    {% for name, type in properties.items() %}
-    {%- set lower = name|lowercase_first_letter %}
-    {%- set upper = name|uppercase_first_letter %}
-    public {{ type }} get{{ upper }}() {
-        return this.{{ lower }};
-    }
-
-    public void set{{ upper }}({{ type }} {{ lower }}) {
-        this.{{ lower }} = {{ lower }};
-    }
-
-    public {{ pojo_name }} with{{ upper }}({{ type }} {{ lower }}) {
-        this.{{ lower }} = {{ lower }};
-        return this;
-    }
-    {% endfor %}
-
-    public {{ pojo_name }}() { }
 }
