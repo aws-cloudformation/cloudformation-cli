ea3c89e2375c203187d7addbc11665a438e827e8
John Tompkins <jotompki@amazon.com>
2019-01-15 12:51:15 -0800

    Unit tests

--- tests/test_command_commons.py
+++ tests/test_command_commons.py
@@ -1,7 +1,6 @@
 from unittest.mock import Mock, patch
 
 import pytest
-from jsonschema.exceptions import ValidationError
 
 from rpdk.cli import EXIT_UNHANDLED_EXCEPTION, main
 from rpdk.project import Project
@@ -18,52 +17,6 @@ def test_command_help(capsys, command):
     out, _ = capsys.readouterr()
     assert "--help" in out
     mock_func.assert_not_called()
-
-
-@pytest.mark.parametrize("command", ["generate", "package", "validate", "submit"])
-def test_command_project_not_found(capsys, command):
-    mock_project = Mock(spec=Project)
-    mock_project.load_settings.side_effect = FileNotFoundError
-
-    with patch(
-        "rpdk.{0}.Project".format(command), autospec=True, return_value=mock_project
-    ):
-        with pytest.raises(SystemExit) as excinfo:
-            main(args_in=[command])
-
-    assert excinfo.value.code != EXIT_UNHANDLED_EXCEPTION
-    mock_project.load_settings.assert_called_once_with()
-    mock_project.load_schema.assert_not_called()
-    try:
-        getattr(mock_project, command).assert_not_called()
-    except AttributeError:
-        pass
-    out, err = capsys.readouterr()
-    assert not err
-    assert "not found" in out
-    assert "init" in out
-
-
-@pytest.mark.parametrize("command", ["generate", "validate"])
-def test_command_invalid_schema(capsys, command):
-    mock_project = Mock(spec=Project)
-    mock_project.load_schema.side_effect = ValidationError("")
-
-    with patch(
-        "rpdk.{0}.Project".format(command), autospec=True, return_value=mock_project
-    ):
-        with pytest.raises(SystemExit):
-            main(args_in=[command])
-    mock_project.load_settings.assert_called_once_with()
-    mock_project.load_schema.assert_called_once_with()
-    try:
-        getattr(mock_project, command).assert_not_called()
-    except AttributeError:
-        pass
-
-    out, err = capsys.readouterr()
-    assert not err
-    assert "invalid" in out
 
 
 @pytest.mark.parametrize("command", ["package", "submit"])
--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,27 +1,7 @@
 from unittest.mock import Mock, patch
 
-import pytest
-
 from rpdk.cli import main
 from rpdk.project import Project
-
-
-def test_generate_command_schema_not_found(capsys):
-    mock_project = Mock(spec=Project)
-    mock_project.load_schema.side_effect = FileNotFoundError
-
-    with patch("rpdk.generate.Project", autospec=True, return_value=mock_project):
-        with pytest.raises(SystemExit) as excinfo:
-            main(args_in=["generate"])
-
-    assert excinfo.value.code == 1
-    mock_project.load_settings.assert_called_once_with()
-    mock_project.load_schema.assert_called_once_with()
-    mock_project.generate.assert_not_called()
-
-    out, err = capsys.readouterr()
-    assert not err
-    assert "not found" in out
 
 
 def test_generate_command_generate(capsys):
@@ -30,8 +10,7 @@ def test_generate_command_generate(capsys):
     with patch("rpdk.generate.Project", autospec=True, return_value=mock_project):
         main(args_in=["generate"])
 
-    mock_project.load_settings.assert_called_once_with()
-    mock_project.load_schema.assert_called_once_with()
+    mock_project.load.assert_called_once_with()
     mock_project.generate.assert_called_once_with()
 
     out, err = capsys.readouterr()
