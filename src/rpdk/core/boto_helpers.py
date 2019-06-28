6f37a30790e2168ecb764ee2771314c586d7d227
Toby Fleming <tobflem@amazon.com>
2019-06-27 17:00:42 -0700

    Get temporary credentials from STS (if necessary)

--- src/rpdk/core/boto_helpers.py
+++ src/rpdk/core/boto_helpers.py
@@ -6,6 +6,9 @@ from .exceptions import CLIMisconfiguredError
 
 LOG = logging.getLogger(__name__)
 
+BOTO_CRED_KEYS = ("aws_access_key_id", "aws_secret_access_key", "aws_session_token")
+LOWER_CAMEL_CRED_KEYS = ("accessKeyId", "secretAccessKey", "sessionToken")
+
 
 def create_sdk_session(region_name=None):
     def _known_error(msg):
@@ -23,3 +26,15 @@ def create_sdk_session(region_name=None):
         _known_error("No credentials specified")
 
     return session
+
+
+def get_temporary_credentials(session, key_names=BOTO_CRED_KEYS):
+    frozen = session.get_credentials().get_frozen_credentials()
+    if frozen.token:
+        creds = (frozen.access_key, frozen.secret_key, frozen.token)
+    else:
+        sts_client = session.client("sts")
+        response = sts_client.get_session_token()
+        temp = response["Credentials"]
+        creds = (temp["AccessKeyId"], temp["SecretAccessKey"], temp["SessionToken"])
+    return dict(zip(key_names, creds))
--- tests/test_boto_helpers.py
+++ tests/test_boto_helpers.py
@@ -1,8 +1,14 @@
-from unittest.mock import patch
+from unittest.mock import create_autospec, patch
 
 import pytest
+from boto3 import Session
 
-from rpdk.core.boto_helpers import create_sdk_session
+from rpdk.core.boto_helpers import (
+    BOTO_CRED_KEYS,
+    LOWER_CAMEL_CRED_KEYS,
+    create_sdk_session,
+    get_temporary_credentials,
+)
 from rpdk.core.exceptions import CLIMisconfiguredError
 
 
@@ -40,3 +46,49 @@ def test_create_sdk_session_no_creds():
             create_sdk_session()
 
     mock_boto3.assert_called_once_with(region_name=None)
+
+
+def test_get_temporary_credentials_has_token():
+    session = create_autospec(spec=Session, spec_set=True)
+    frozen = session.get_credentials.return_value.get_frozen_credentials.return_value
+    frozen.access_key = object()
+    frozen.secret_key = object()
+    frozen.token = object()
+
+    creds = get_temporary_credentials(session)
+
+    session.get_credentials.assert_called_once_with()
+    session.client.assert_not_called()
+
+    assert len(creds) == 3
+    assert tuple(creds.keys()) == BOTO_CRED_KEYS
+    assert tuple(creds.values()) == (frozen.access_key, frozen.secret_key, frozen.token)
+
+
+def test_get_temporary_credentials_needs_token():
+    session = create_autospec(spec=Session, spec_set=True)
+    frozen = session.get_credentials.return_value.get_frozen_credentials.return_value
+    frozen.token = None
+
+    access_key = object()
+    secret_key = object()
+    token = object()
+
+    client = session.client.return_value
+    client.get_session_token.return_value = {
+        "Credentials": {
+            "AccessKeyId": access_key,
+            "SecretAccessKey": secret_key,
+            "SessionToken": token,
+        }
+    }
+
+    creds = get_temporary_credentials(session, LOWER_CAMEL_CRED_KEYS)
+
+    session.get_credentials.assert_called_once_with()
+    session.client.assert_called_once_with("sts")
+    client.get_session_token.assert_called_once_with()
+
+    assert len(creds) == 3
+    assert tuple(creds.keys()) == LOWER_CAMEL_CRED_KEYS
+    assert tuple(creds.values()) == (access_key, secret_key, token)
