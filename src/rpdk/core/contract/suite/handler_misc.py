be77efce514581c6fb85611c32adb6408c98295e
Toby Fleming <tobflem@amazon.com>
2019-06-27 17:23:47 -0700

    Remove deprecated pytest.raises(x, message=y)
    See https://github.com/pytest-dev/pytest/issues/3974

--- src/rpdk/core/contract/suite/handler_misc.py
+++ src/rpdk/core/contract/suite/handler_misc.py
@@ -8,5 +8,6 @@ def contract_check_asserts_work():
         "contract tests will not run correctly. Please raise an issue with "
         "as much information about your system and runtime as possible."
     )
-    with pytest.raises(AssertionError, message=message):
+    with pytest.raises(AssertionError):
         assert False  # noqa: B011
+        pytest.fail(message)
