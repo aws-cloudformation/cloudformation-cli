110c69bc469d11ea202f04f12cff7bde53b801fe
Adam Daily <adaily@amazon.com>
2019-09-27 09:46:48 -0700

    Fix bugs related to contract tests against UpdateHandler-less resources (#294)
    * Omit update NotFound tests when update handler is non-existent
    
    Currently resources will return an InternalFailure if update handlers
    don't exist and we try to check for NotFound on an update against a
    deleted resource. This change fixes that.
    
    * Use updated model for deletion NotFound contract tests
    
    Some delete handlers accept readOnly properties to attempt to delete a
    resource. If we use the initial create model in the contract tests,
    those delete handlers may fail superficial parameter validation in API
    delete requests that don't allow for null values. This results in the
    handler returning an InternalFailure status code rather than a NotFound
    status code.
    
    This change uses the updated model from previous tests to attempt the
    delete request since that model will have readOnly properties necessary
    to make the delete API call.

--- src/rpdk/core/contract/suite/handler_misc.py
+++ src/rpdk/core/contract/suite/handler_misc.py
@@ -67,9 +67,11 @@ def contract_crud_exerciser(resource_client):
     list_models = _test_list_success(resource_client, updated_model)
     assert updated_model not in list_models
 
-    _test_update_failure_not_found(resource_client, updated_model)
+    if resource_client.has_update_handler():
+        _test_update_failure_not_found(resource_client, updated_model)
+
     # DELETE: Should fail with NotFound because we've already deleted the resource.
-    _test_delete_failure_not_found(resource_client, create_model)
+    _test_delete_failure_not_found(resource_client, updated_model)
 
     new_create_model = create_model
     try:
