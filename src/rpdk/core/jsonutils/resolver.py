8f8ef4ab5015b173e91f4ff14049fb19d0b233b5
Andrew Goldberg <goldband@amazon.com>
2019-10-28 12:43:23 -0700

    default insertionOrder to True and add support for MULTI_SET and ORDERED_SET

--- src/rpdk/core/jsonutils/resolver.py
+++ src/rpdk/core/jsonutils/resolver.py
@@ -14,6 +14,8 @@ class ContainerType(Enum):
     DICT = auto()
     LIST = auto()
     SET = auto()
+    MULTI_SET = auto()
+    ORDERED_SET = auto()
 
 
 class ResolvedType:
@@ -109,12 +111,19 @@ class ModelResolver:
     @staticmethod
     def _get_array_container_type(property_schema):
         """Return True if an array has array semantics, or False for set semantics."""
-        insertion_order = property_schema.get("insertionOrder", False)
+        insertion_order = property_schema.get("insertionOrder", True)
         unique_items = property_schema.get("uniqueItems", False)
 
-        if insertion_order or not unique_items:
-            return ContainerType.LIST
-        return ContainerType.SET
+        if insertion_order:
+            if unique_items:  # pylint: disable=no-else-return
+                return ContainerType.ORDERED_SET
+            else:
+                return ContainerType.LIST
+        else:
+            if unique_items:  # pylint: disable=no-else-return
+                return ContainerType.SET
+            else:
+                return ContainerType.MULTI_SET
 
     @staticmethod
     def _get_primitive_lang_type(schema_type):
