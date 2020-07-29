e04dfe368dd359fe7fc5f88c0bd770e054106486
Anton Mokhovikov <ammokhov@amazon.com>
2020-07-28 23:28:22 -0700

    added a writable id skip for read without create

--- src/rpdk/core/contract/suite/handler_read.py
+++ src/rpdk/core/contract/suite/handler_read.py
@@ -5,10 +5,12 @@ import pytest
 
 # WARNING: contract tests should use fully qualified imports to avoid issues
 # when being loaded by pytest
+from rpdk.core.contract.suite.contract_asserts import skip_not_writable_identifier
 from rpdk.core.contract.suite.handler_commons import test_read_failure_not_found
 
 
 @pytest.mark.read
+@skip_not_writable_identifier
 def contract_read_without_create(resource_client):
     model = resource_client.generate_create_example()
     test_read_failure_not_found(resource_client, model)
