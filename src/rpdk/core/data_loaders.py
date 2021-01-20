197b3b176eeaeb4289dfc1a5b5858999b73950b4
Pat Myron <PatMyron@users.noreply.github.com>
2021-01-19 17:45:32 -0800

    catch common resource schema issues (#663)
    resource types with LIST API options like MaxResults, NextToken, Filters, etc as properties of the resource itself:
    https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-licensemanager/pull/3
    https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-iotwireless/pull/3
    https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-ssm/pull/75
    
    readOnlyProperties overlapping with writeOnlyProperties or createOnlyProperties:
    https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-auditmanager/pull/2
    https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-codeartifact/pull/38
    
    no wildcards in handler permissions:
    https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-greengrassv2/pull/4
    https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-imagebuilder/commit/55fa9bfa47406e29def2bf1812fbce8cc7c17b8b#r46035310

--- src/rpdk/core/data_loaders.py
+++ src/rpdk/core/data_loaders.py
@@ -123,14 +123,7 @@ def get_file_base_uri(file):
     return path.resolve().as_uri()
 
 
-def _is_in(schema, key):
-    def contains(value):
-        return value in schema.get(key, [])
-
-    return contains
-
-
-def load_resource_spec(resource_spec_file):  # noqa: C901
+def load_resource_spec(resource_spec_file):  # pylint: disable=R0912 # noqa: C901
     """Load a resource provider definition from a file, and validate it."""
     try:
         resource_spec = json.load(resource_spec_file)
@@ -148,6 +141,40 @@ def load_resource_spec(resource_spec_file):  # noqa: C901
         LOG.debug("Resource spec validation failed", exc_info=True)
         raise SpecValidationError(str(e)) from e
 
+    list_options = {
+        "maxresults",
+        "maxrecords",
+        "maxitems",
+        "nexttoken",
+        "nextmarker",
+        "nextpagetoken",
+        "pagetoken",
+        "paginationtoken",
+    } & set(map(str.lower, resource_spec.get("properties", [])))
+    if list_options:
+        LOG.warning(
+            "LIST API inputs like MaxResults, MaxRecords, MaxItems, NextToken, NextMarker, NextPageToken, PageToken, and Filters are not resource properties. \
+%s should not be present in resource schema",
+            list_options,
+        )
+
+    if set(resource_spec.get("readOnlyProperties", [])) & set(
+        resource_spec.get("createOnlyProperties", [])
+    ) or set(resource_spec.get("readOnlyProperties", [])) & set(
+        resource_spec.get("writeOnlyProperties", [])
+    ):
+        LOG.warning(
+            "readOnlyProperties cannot be specified by customers and should not overlap with writeOnlyProperties or createOnlyProperties"
+        )
+
+    for handler in resource_spec.get("handlers", []):
+        for permission in resource_spec.get("handlers", [])[handler]["permissions"]:
+            if "*" in permission:
+                LOG.warning(
+                    "Use specific handler permissions instead of using wildcards: %s",
+                    permission,
+                )
+
     try:
         additional_properties_validator.validate(resource_spec)
     except ValidationError as e:
@@ -158,13 +185,11 @@ property is of object type and has properties or patternProperties defined \
 in it. Please fix the warnings: %s",
             str(e),
         )
-    in_readonly = _is_in(resource_spec, "readOnlyProperties")
-    in_createonly = _is_in(resource_spec, "createOnlyProperties")
 
-    primary_ids = resource_spec["primaryIdentifier"]
-
-    for primary_id in primary_ids:
-        if not in_readonly(primary_id) and not in_createonly(primary_id):
+    for primary_id in resource_spec["primaryIdentifier"]:
+        if primary_id not in resource_spec.get(
+            "readOnlyProperties", []
+        ) and primary_id not in resource_spec.get("createOnlyProperties", []):
             LOG.warning(
                 "Property 'primaryIdentifier' - %s must be specified \
 as either readOnly or createOnly",
