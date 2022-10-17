76c1236e998054023b4d830ee99121c8747c03d9
Kevin DeJong <kddejong@amazon.com>
2022-10-17 15:25:43 -0700

    Add python 3.9 and 3.10 support

--- setup.py
+++ setup.py
@@ -73,6 +73,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="Amazon Web Services AWS CloudFormation",
 )
