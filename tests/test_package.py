b64043e202765e7cd95201d2d950fb3dfec88c5c
Michael Maeng <mmaeng@amazon.com>
2022-11-03 23:17:32 -0400

    remove unnecessary argument for test

--- tests/test_package.py
+++ tests/test_package.py
@@ -4,14 +4,18 @@ from rpdk.core.cli import main
 from rpdk.core.project import Project
 
 
-def test_package_command_valid_schema(capsys):
+def test_package_command_valid_schema():
     mock_project = Mock(spec=Project)
 
     with patch("rpdk.core.package.Project", autospec=True, return_value=mock_project):
         main(args_in=["package"])
 
-    mock_project.load.assert_called_once_with()
-
-    out, err = capsys.readouterr()
-    assert not err
-    assert "failed" not in out
+    mock_project.load.assert_called_once()
+    mock_project.submit.assert_called_once_with(
+        dry_run=True,
+        endpoint_url=False,
+        region_name=False,
+        role_arn=False,
+        use_role=False,
+        set_default=False,
+    )
