f12b797fc21579bd71a01ac2008ae2905d05c154
Michael Maeng (AWS) <83842446+mmaeng@users.noreply.github.com>
2022-11-10 20:10:24 +0000

    fix(contract): added regex for JSON string formats date-time, date, time, and email  (#930)
    * Added regex for JSON string formats date-time, date, email, and time
    
    * When generating an example resource, return ASCII for strings
    
    * Added start string replacement for terminate_regex and tests

--- tests/contract/test_resource_generator.py
+++ tests/contract/test_resource_generator.py
@@ -22,9 +22,28 @@ def test_terminate_regex_end_of_line_like_a_normal_person():
     assert re.match(modified_regex, "dfqh3eqefhq")
 
 
-def test_terminate_regex_no_termination_needed():
+def test_terminate_regex_line_start_change():
     original_regex = r"^[a-zA-Z0-9]{1,219}\Z"
-    assert terminate_regex(original_regex) == original_regex
+    terminated_regex = r"\A[a-zA-Z0-9]{1,219}\Z"
+    assert terminate_regex(original_regex) == terminated_regex
+
+
+def test_terminate_regex_line_end_change():
+    original_regex = r"\A[a-zA-Z0-9]{1,219}$"
+    terminated_regex = r"\A[a-zA-Z0-9]{1,219}\Z"
+    assert terminate_regex(original_regex) == terminated_regex
+
+
+def test_terminate_regex_line_start_and_end_change():
+    original_regex = r"^[a-zA-Z0-9]{1,219}$"
+    terminated_regex = r"\A[a-zA-Z0-9]{1,219}\Z"
+    assert terminate_regex(original_regex) == terminated_regex
+
+
+def test_terminate_regex_no_termination_needed():
+    original_regex = r"\A[a-zA-Z0-9]{1,219}\Z"
+    terminated_regex = r"\A[a-zA-Z0-9]{1,219}\Z"
+    assert terminate_regex(original_regex) == terminated_regex
 
 
 @pytest.mark.parametrize("schema_type", ["integer", "number"])
@@ -79,6 +98,22 @@ def test_generate_string_strategy_format():
     schema = {"type": "string", "format": "arn"}
     strategy = ResourceGenerator(schema).generate_schema_strategy(schema)
     assert re.fullmatch(STRING_FORMATS["arn"], strategy.example())
+
+    schema = {"type": "string", "format": "date-time"}
+    strategy = ResourceGenerator(schema).generate_schema_strategy(schema)
+    assert re.match(STRING_FORMATS["date-time"], strategy.example())
+
+    schema = {"type": "string", "format": "time"}
+    strategy = ResourceGenerator(schema).generate_schema_strategy(schema)
+    assert re.match(STRING_FORMATS["time"], strategy.example())
+
+    schema = {"type": "string", "format": "date"}
+    strategy = ResourceGenerator(schema).generate_schema_strategy(schema)
+    assert re.match(STRING_FORMATS["date"], strategy.example())
+
+    schema = {"type": "string", "format": "email"}
+    strategy = ResourceGenerator(schema).generate_schema_strategy(schema)
+    assert re.match(STRING_FORMATS["email"], strategy.example())
 
 
 def test_generate_string_strategy_length():
