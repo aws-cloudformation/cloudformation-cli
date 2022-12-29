2b975032ef23a94170a93a506deb62ae7a927d2d
Michael Maeng <mmaeng@amazon.com>
2022-12-29 17:42:08 -0500

    Add in typeconfig option override and unittests

--- src/rpdk/core/contract/type_configuration.py
+++ src/rpdk/core/contract/type_configuration.py
@@ -6,48 +6,50 @@ from rpdk.core.exceptions import InvalidProjectError
 
 LOG = logging.getLogger(__name__)
 
-TYPE_CONFIGURATION_FILE_PATH = "~/.cfn-cli/typeConfiguration.json"
-
 
 class TypeConfiguration:
     TYPE_CONFIGURATION = None
 
     @staticmethod
-    def get_type_configuration():
+    def get_type_configuration(typeconfigloc):
+        if typeconfigloc:
+            type_config_file_path = typeconfigloc
+        else:
+            type_config_file_path = "~/.cfn-cli/typeConfiguration.json"
+
         LOG.debug(
-            "Loading type configuration setting file at '~/.cfn-cli/typeConfiguration.json'"
+            "Loading type configuration setting file at %s",
+            type_config_file_path,
         )
         if TypeConfiguration.TYPE_CONFIGURATION is None:
             try:
                 with open(
-                    os.path.expanduser(TYPE_CONFIGURATION_FILE_PATH), encoding="utf-8"
+                    os.path.expanduser(type_config_file_path), encoding="utf-8"
                 ) as f:
                     TypeConfiguration.TYPE_CONFIGURATION = json.load(f)
             except json.JSONDecodeError as json_decode_error:
                 LOG.debug(
                     "Type configuration file '%s' is invalid",
-                    TYPE_CONFIGURATION_FILE_PATH,
+                    type_config_file_path,
                 )
                 raise InvalidProjectError(
-                    "Type configuration file '%s' is invalid"
-                    % TYPE_CONFIGURATION_FILE_PATH
+                    "Type configuration file '%s' is invalid" % type_config_file_path
                 ) from json_decode_error
             except FileNotFoundError:
                 LOG.debug(
                     "Type configuration file '%s' not Found, do nothing",
-                    TYPE_CONFIGURATION_FILE_PATH,
+                    type_config_file_path,
                 )
         return TypeConfiguration.TYPE_CONFIGURATION
 
     @staticmethod
-    def get_hook_configuration():
-        # pylint: disable=unsubscriptable-object
-        type_configuration = TypeConfiguration.get_type_configuration()
+    def get_hook_configuration(typeconfigloc):
+        type_configuration = TypeConfiguration.get_type_configuration(typeconfigloc)
         if type_configuration:
             try:
-                return type_configuration["CloudFormationConfiguration"][
+                return type_configuration.get("CloudFormationConfiguration", {})[
                     "HookConfiguration"
-                ].get("Properties")
+                ]["Properties"]
             except KeyError as e:
                 LOG.warning("Hook configuration is invalid")
                 raise InvalidProjectError("Hook configuration is invalid") from e
