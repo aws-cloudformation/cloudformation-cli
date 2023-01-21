2744409ca582df1be5f97ab95b8226468ce43779
Nick Carpenter <carpnick@users.noreply.github.com>
2023-01-21 13:17:57 -0500

    Update handler_update_invalid.py

--- src/rpdk/core/contract/suite/resource/handler_update_invalid.py
+++ src/rpdk/core/contract/suite/resource/handler_update_invalid.py
@@ -10,18 +10,24 @@ from rpdk.core.contract.suite.contract_asserts_commons import failed_event
 
 
 @pytest.mark.update
+def contract_update_without_create(resource_client):
+    test_update_without_create(resource_client)
+
+
 @failed_event(
     error_code=HandlerErrorCode.NotFound,
     msg="An update handler MUST return FAILED with a NotFound error code\
          if the resource did not exist prior to the update request",
 )
-def contract_update_without_create(resource_client):
+def test_update_without_create(resource_client):
     create_request = resource_client.generate_create_example()
     update_request = resource_client.generate_update_example(create_request)
     _status, response, _error = resource_client.call_and_assert(
-        Action.UPDATE, OperationStatus.FAILED, update_request, create_request
+      Action.UPDATE, OperationStatus.FAILED, update_request, create_request
     )
     assert response[
-        "message"
+      "message"
     ], "The progress event MUST return an error message\
          when the status is failed"
+
+    return _error
