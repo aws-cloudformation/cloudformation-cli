d24cb93cc864533fba29ae112be7b982e77f19bd
Adrian Chouza <achouza@amazon.com>
2023-08-23 13:47:43 -0700

    format: fixing black formatting

--- src/rpdk/core/plugin_registry.py
+++ src/rpdk/core/plugin_registry.py
@@ -22,6 +22,7 @@ def get_parsers():
 
     return parsers
 
+
 def get_extensions():
     extensions = {
         entry_point.name: entry_point.load
@@ -29,6 +30,7 @@ def get_extensions():
     }
 
     return extensions
+
 
 def load_plugin(language):
     return PLUGIN_REGISTRY[language]()()
