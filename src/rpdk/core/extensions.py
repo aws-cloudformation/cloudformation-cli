37b45ce2888cc2dc0ad17b919d4adbdf094037f8
Adrian Chouza <achouza@amazon.com>
2023-08-28 11:36:34 -0700

    code: safety check for collisions

--- src/rpdk/core/extensions.py
+++ src/rpdk/core/extensions.py
@@ -1,10 +1,18 @@
 from .plugin_registry import get_extensions
 
 
+def _check_command_name_collision(subparsers, command_name):
+    if command_name in subparsers.choices:
+        raise RuntimeError(
+            f'"{command_name}" is already registered as an extension. Please use a different name.'
+        )
+
+
 def setup_subparsers(subparsers, parents):
     extensions = get_extensions()
 
     for extension_cls in extensions.values():
         extension = extension_cls()()
+        _check_command_name_collision(subparsers, extension.command_name)
         parser = subparsers.add_parser(extension.command_name, parents=parents)
         extension.setup_parser(parser)
--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -1,25 +1,60 @@
+import argparse
+from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
 from rpdk.core.extensions import setup_subparsers
 
 
-def test_setup_subparsers():
-    expeted_command_name = "expected-command-name"
+class ExtensionTest(TestCase):
+    def test_setup_subparsers(self):  # pylint: disable=no-self-use
+        expeted_command_name = "expected-command-name"
 
-    mock_extension = MagicMock()
-    mock_extension.command_name = expeted_command_name
+        mock_extension = MagicMock()
+        mock_extension.command_name = expeted_command_name
 
-    mock_extension_entry_point = MagicMock()
-    mock_extension_entry_point.return_value.return_value = mock_extension
+        mock_extension_entry_point = MagicMock()
+        mock_extension_entry_point.return_value.return_value = mock_extension
 
-    mock_extension_entry_points = {"key": mock_extension_entry_point}
+        mock_extension_entry_points = {"key": mock_extension_entry_point}
 
-    subparsers, parents, parser = MagicMock(), MagicMock(), MagicMock()
-    subparsers.add_parser.return_value = parser
+        subparsers, parents, parser = MagicMock(), MagicMock(), MagicMock()
+        subparsers.add_parser.return_value = parser
 
-    with patch("rpdk.core.extensions.get_extensions") as mock_get_extensions:
-        mock_get_extensions.return_value = mock_extension_entry_points
-        setup_subparsers(subparsers, parents)
+        with patch("rpdk.core.extensions.get_extensions") as mock_get_extensions:
+            mock_get_extensions.return_value = mock_extension_entry_points
+            setup_subparsers(subparsers, parents)
 
-    mock_extension.setup_parser.assert_called_once_with(parser)
-    subparsers.add_parser.assert_called_with(expeted_command_name, parents=parents)
+        mock_extension.setup_parser.assert_called_once_with(parser)
+        subparsers.add_parser.assert_called_with(expeted_command_name, parents=parents)
+
+    def test_setup_subparsers_should_raise_error_when_collision_occur(self):
+        command_name = "command-name"
+
+        mock_extension_1, mock_extension_2 = MagicMock(), MagicMock()
+        mock_extension_1.command_name = command_name
+        mock_extension_2.command_name = command_name
+
+        mock_extension_1_entry_point = MagicMock()
+        mock_extension_1_entry_point.return_value.return_value = mock_extension_1
+
+        mock_extension_2_entry_point = MagicMock()
+        mock_extension_2_entry_point.return_value.return_value = mock_extension_2
+
+        mock_extension_entry_points = {
+            "key_1": mock_extension_1_entry_point,
+            "key_2": mock_extension_2_entry_point,
+        }
+
+        parser = argparse.ArgumentParser()
+        subparsers = parser.add_subparsers()
+
+        with patch(
+            "rpdk.core.extensions.get_extensions"
+        ) as mock_get_extensions, self.assertRaises(RuntimeError) as context:
+            mock_get_extensions.return_value = mock_extension_entry_points
+            setup_subparsers(subparsers, [])
+
+        assert (
+            str(context.exception)
+            == '"command-name" is already registered as an extension. Please use a different name.'
+        )
