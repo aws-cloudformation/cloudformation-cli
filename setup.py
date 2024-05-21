679e2fa49a6c275fe016a45d05e6c0754230e7ee
Anton Mokhovikov <ammokhov@amazon.com>
2024-05-21 14:43:55 -0700

    Added SGR validation to `cfn validate`

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         "cfn_flip>=1.2.3",
         "nested-lookup",
         "botocore>=1.31.17",
+        "resource-schema-guard-rail>=0.0.12",
     ],
     entry_points={
         "console_scripts": ["cfn-cli = rpdk.core.cli:main", "cfn = rpdk.core.cli:main"]
